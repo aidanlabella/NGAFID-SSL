4b26b7b05850a920d701d55c79e6cdea7a0032fd
Tejadi <adigsram@gmail.com>
2024-11-22 11:28:09 -0500

    bert_simclr update

--- models/bert_simclr.py
+++ models/bert_simclr.py
@@ -1,45 +1,46 @@
+import torch
 import torch.nn as nn
-from transformers import BertModel, BertTokenizer
+from transformers import BertModel
 
 class BERTSimCLR(nn.Module):
-    def __init__(self, out_dim):
+    def __init__(self, out_dim, max_len=10000):
+
         super(BERTSimCLR, self).__init__()
         
-        # Initialize BERT model and tokenizer
         self.bert = BertModel.from_pretrained("bert-base-uncased")
-        self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-        
-        # Get the dimension of BERT’s last hidden state output
-        dim_mlp = self.bert.config.hidden_size  # Typically 768 for bert-base-uncased
 
-        # Add MLP projection head
+        self.bert.embeddings.position_embeddings = nn.Embedding(max_len, self.bert.config.hidden_size)
+        self.bert.config.max_position_embeddings = max_len  
+
+        self.input_mapping = nn.Linear(44, self.bert.config.hidden_size)
+
+        dim_mlp = self.bert.config.hidden_size  
         self.projection_head = nn.Sequential(
             nn.Linear(dim_mlp, dim_mlp),
             nn.ReLU(),
             nn.Linear(dim_mlp, out_dim)
         )
 
-    def forward(self, input_text):
-        # Tokenize input text and get token IDs and attention mask
-        inputs = self.tokenizer(input_text, return_tensors="pt", padding=True, truncation=True)
-        outputs = self.bert(**inputs)
+    def forward(self, input_data):
 
-        # Use the pooled output (for classification) or last hidden state as needed
+        batch_size, seq_len, _ = input_data.size()
+        
+        input_data_mapped = self.input_mapping(input_data)
+        
+        position_ids = torch.arange(seq_len, dtype=torch.long, device=input_data.device).unsqueeze(0).expand(batch_size, -1)
+        
+        token_type_ids = torch.zeros((batch_size, seq_len), dtype=torch.long, device=input_data.device)
+        
+        outputs = self.bert(inputs_embeds=input_data_mapped, position_ids=position_ids, token_type_ids=token_type_ids)
+        
         pooled_output = outputs.pooler_output
+        
+        print(pooled_output.shape)
+
         return self.projection_head(pooled_output)
 
-    # @staticmethod
-    def _get_baseline(out_dim):
-        """
-        Create a baseline instance of the BERTSimCLR model with the specified output dimension.
 
-        Args:
-            out_dim (int): The output dimension for the projection head.
+    def remove_projector(self):
+        self.projection_head = nn.Identity()
 
-        Returns:
-            BERTSimCLR: An instance of the BERTSimCLR model initialized for the baseline.
-        """
-        return BERTSimCLR(out_dim=out_dim)
 
-# Example usage
-# baseline_model = BERTSimCLR.get_baseline(out_dim=128)
